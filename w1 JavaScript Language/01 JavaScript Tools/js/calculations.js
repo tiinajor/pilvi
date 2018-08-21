801f9958d41c66977e23c92a125fc1d55fb241d2
Tiina Jormalainen <tiinajor@metropolia.fi>
2018-08-21 13:09:57 +0300

    todo 123 done

--- w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/calculations.js
@@ -1,12 +1,44 @@
-function multiplyBy()
-{
-// check the input
-    
+var num1;
+var num2;
+
+var getValues = () => {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
 }
 
-function divideBy() 
-{ 
-// check the input
- 
+var checkInput = () => {
+    if (num1 === '' || num2 === '') return "Täytä kaikki kentät";
+}
+
+var checkNumericValue = () => {
+    var isNaN1 = Number(num1);
+    var isNaN2 = Number(num2);
+    if (isNaN(num1) || isNaN(num2)) return "Piti olla numero";
+}
+
+function multiplyBy() {
+    getValues();
+    var validInputs = checkInput();
+    var numericValue = checkNumericValue();
+
+    if (validInputs || numericValue) {
+        document.getElementById("result").innerHTML = validInputs || numericValue;
+    } else {
+        document.getElementById("result").innerHTML = num1 * num2;
+    }
+}
+
+function divideBy() {
+    getValues();
+    var validInputs = checkInput();
+    var numericValue = checkNumericValue();
+    var divideZero = "Ei voi jakaa nollalla";
+
+    if (validInputs || numericValue || num2 == 0) {
+        document.getElementById("result").innerHTML = validInputs || numericValue || divideZero;
+    } else {
+        document.getElementById("result").innerHTML = num1 / num2;
+    }
+
 }
 
--- w1 JavaScript Language/01 JavaScript Tools/js/guess.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/guess.js
@@ -1,23 +1,53 @@
-let targetNumber = Math.floor(Math.random() * 10) + 1;
+var wrongAnswers = []
+var targetNumber = () => Math.floor(Math.random() * 10) + 1;
 
-function init () {
-	console.log("You have to code this!");
+function init() {
+	
+	document.getElementById("myForm").addEventListener("submit", function (event) {
+		event.preventDefault();
+		var value = this.children[0].value
+		if(!isNaN(value) && value !== ''){
+			if(value > 0 && value <= 10){
+				check(value)
+			}else {
+				this.children[0].value = "Between 1 & 10"
+			}
+		
+		}else{
+			this.children[0].value = "we need a number!"
+		}
+		
+	});
 }
 
-function check (value) {
-  
+function check(value) {
+	var correct = targetNumber();
+	if (correct == value) {
+		wrongAnswers = []
+		showWin()
+	} else {
+		wrongAnswers.push(value)
+		if (wrongAnswers.length >= 5) {
+			wrongAnswers = []
+			showLoss()
+		} else {
+			showError()
+		}
+	}
 }
 
-function showWin () {
-  
+function showWin() {
+	//document.getElementById('status').innerText = "win"
+	document.body.innerHTML = "<h1>WIN</h1>"
 }
 
-function showError () {
-  
+function showError() {
+	document.getElementById('message').innerText = "nope"
 }
 
-function showLoss () {
-  
+function showLoss() {
+	document.body.innerHTML = "<h1>LOSE</h1>"
+
 }
 
 init();
